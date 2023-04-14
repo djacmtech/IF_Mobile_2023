99856ae3a728245320b175ce09b8c056d94e38e3
Sonia D'Silva <sonia.dsilva.2003@gmail.com>
2023-04-14 19:36:40 +0530

    Updated sign up page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:internship_fair/screens/cart.dart';
 import 'package:internship_fair/screens/login.dart';
 
 void main() {
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  const MaterialApp(
-      home: LoginScreen(),
+      home: MyCart(),
     );
   }
 }
